2ada90c9f46dad1964595ca93013e941345d140f
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2025-03-20 16:00:23 +0100

    Add files via upload

---
+++ SpaceForceGame/index.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Main. SpaceForceGame
+ * 
+ * @package SpaceForceGame
+ */
+get_header();
+?>
+
+<?php wp_head(); ?>
+
+
+
+<?php
+
+// get_template_part('alerts/fullScreen');
+
+// get_template_part('menu/navbar');
+
+// get_template_part('pages/app-content');
+
+echo '<div class="player"></div>';
+
+?>
+
+
+
+<?php wp_footer(); ?>
+
+<?php get_footer(); ?>
---
+++ SpaceForceGame/pages/run/run.js
@@ -0,0 +1,5 @@
+const player = new Player('.player');
+player.setAEL();
+//player.removeAEL();  // usunięcie eventListenerów tej klasy
+//player.removeWorker();  // usunięcie workera tej klasy
+//player = null;  // usunięcie klasy
