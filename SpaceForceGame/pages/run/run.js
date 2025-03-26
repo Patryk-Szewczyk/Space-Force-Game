8373adc0a9072075145ad6a3189dc86c0d6a1705
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2025-03-26 20:03:29 +0100

    Delete SpaceForceGame directory

--- SpaceForceGame/pages/run/run.js
+++
@@ -1,5 +0,0 @@
-const player = new Player('.player');
-player.setAEL();
-//player.removeAEL();  // usunięcie eventListenerów tej klasy
-//player.removeWorker();  // usunięcie workera tej klasy
-//player = null;  // usunięcie klasy
